2a2e05cb61b49a4c0b7af1c50f62ae2cd0eecb7a
novian <kingboard154@example.com>
2024-02-16 18:58:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: '#262626',
+        secondary: '#ffffff',
+        tertiary: '#99CB3C'
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
+
